8a0955e248517a9782d7cd66130aaebd9cf920d4
Kiran Babu Rajanala <rajanalakiranbabu@gmail.com>
2017-01-23 12:37:21 -0500

    Initial Commit

---
+++ newrep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newrep
+//
+//  Created by Kiranbabu Rajanala on 23/1/17.
+//  Copyright © 2017 Kiranbabu Rajanala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
